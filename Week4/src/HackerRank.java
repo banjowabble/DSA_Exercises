2204b243281e767df455941c91d8d9c8add591de
Phan Thanh Ngọc <thanhngoc.2002.vn@gmail.com>
2021-10-05 22:54:59 +0700

    Add 5 more linked list functions I did on HackerRank

--- Week4/src/HackerRank.java
+++ Week4/src/HackerRank.java
@@ -116,4 +116,108 @@ public class HackerRank {
         llist = prev;
         return llist;
     }
+
+    /**
+     * compare two linked list.
+     * @param head1 head of the first list
+     * @param head2 head of the second list
+     * @return boolean check
+     */
+    static boolean compareLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        SinglyLinkedListNode p1 = head1;
+        SinglyLinkedListNode p2 = head2;
+        while(p1 != null && p2 != null && p1.data == p2.data) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p1 == p2;
+    }
+
+    /**
+     * merge two sorted lists.
+     * @param head1 head of the first list
+     * @param head2 head of the second list
+     * @return head of the newly merged list
+     */
+    static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        SinglyLinkedListNode result;
+        if (head1 == null) return head2;
+        else if (head2 == null) return head1;
+
+        if (head1.data >= head2.data)
+        {
+            result = head2;
+            result.next = mergeLists(head1, head2.next);
+        }
+        else
+        {
+            result = head1;
+            result.next = mergeLists (head1.next, head2);
+        }
+        return result;
+    }
+
+    /**
+     * get data a position counted backwards.
+     * @param llist head of the linked list
+     * @param positionFromTail position from tail
+     * @return data at the desired position
+     */
+    public static int getNode(SinglyLinkedListNode llist, int positionFromTail) {
+        // Write your code here
+        int length = 0;
+        for (SinglyLinkedListNode p = llist ; p != null; p = p.next)
+        {
+            length++;
+        }
+        int positionFromHead = length - 1 - positionFromTail;
+        SinglyLinkedListNode pos = llist;
+        for (int i = 0; i < positionFromHead; i++)
+        {
+            pos = pos.next;
+        }
+        return pos.data;
+    }
+
+    /**
+     * remove duplicates in a linked list
+     * @param llist head of the linked list
+     * @return head of the modified list
+     */
+    public static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode llist) {
+        // Write your code here
+        if (llist == null) return null;
+        SinglyLinkedListNode p = llist;
+        while (p.next != null) {
+            if (p.data == p.next.data) {
+                p.next = p.next.next;
+                continue;
+            }
+            p = p.next;
+        }
+
+        return llist;
+    }
+
+    /**
+     * find the merge point of two linked lists.
+     * @param head1 head of the first list
+     * @param head2 head of the second list
+     * @return data at the merge point
+     */
+    static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        int mergeData = 0;
+        for (SinglyLinkedListNode p = head1 ; p != null; p = p.next)
+        {
+            for (SinglyLinkedListNode q = head2 ; q != null; q = q.next)
+            {
+                if (p == q)
+                {
+                    mergeData = p.data;
+                    return mergeData;
+                }
+            }
+        }
+        return mergeData;
+    }
 }
